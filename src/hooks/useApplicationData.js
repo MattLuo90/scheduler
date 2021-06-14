f34a2547399dbee08422973ca3c972f7e46f7972
MattLuo90 <mattluo604@hotmail.com>
2021-06-13 20:29:23 -0700

    add reducer function

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,13 +1,18 @@
-import React, {useEffect, useState} from "react";
+import {useEffect, useReducer} from "react";
 import axios from 'axios';
-
+import reducer, {
+  SET_APPLICATION_DATA,
+  SET_DAY,
+  SET_INTERVIEW
+} from 'helpers/reducer';
+ 
 
 export default function useApplication() {
   
-  const setDay = day => setState(prev => ({ ...prev, day }));
+  const setDay = day => dispatch({ type: SET_DAY, day: day });
   
   
-  const [state, setState] = useState({
+  const [state, dispatch] = useReducer(reducer, {
     day: 'Monday',
     days: [],
     appointments: {},
@@ -22,14 +27,14 @@ export default function useApplication() {
       axios.get('http://localhost:8001/api/interviewers')
     ])
     .then(all => {
-        setState(prev => ({ 
-          ...prev, 
-          days: all[0].data, 
-          appointments: all[1].data, 
-          interviewers: all[2].data
-        }));
-      })
-  }, [])
+      dispatch({
+        type: SET_APPLICATION_DATA,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data
+      });
+    });
+  }, []);
   
   const bookInterview= (id, interview) => {
     const appointment = {
@@ -59,14 +64,13 @@ export default function useApplication() {
       days.push(newDaysObject[obj])
     }
     
+
+    dispatch({
+      type: SET_INTERVIEW,
+      appointments,
+      days
+    });
     return axios.put(`http://localhost:8001/api/appointments/${id}`, { interview })
-      .then(() => {
-        setState({
-        ...state,
-        appointments,
-        days 
-      })
-    })
   }
   
   function cancelInterview(id) {
@@ -98,14 +102,14 @@ export default function useApplication() {
     for (let obj in newDaysObject) {
       days.push(newDaysObject[obj])
     }
+
+    dispatch({
+      type: SET_INTERVIEW,
+      appointments,
+      days
+    });
     return axios.delete(`http://localhost:8001/api/appointments/${id}`)
-      .then(() => {
-        setState({
-        ...state,
-        appointments,
-        days
-      })
-    })
   }
+
   return {setDay, state, cancelInterview, bookInterview}
 }
